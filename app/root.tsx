20c79e9821d20578bfe870d6bf2aa92cd705b899
TedisAgolli <tedisagolli@gmail.com>
2022-05-05 18:44:28 -0400

    Update title

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix Conf Frames",
   viewport: "width=device-width,initial-scale=1",
 });
 
